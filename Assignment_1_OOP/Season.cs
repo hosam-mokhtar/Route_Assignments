dfb74b14d0ad0acbcf7dcde428e6f8c16ad24eda
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-07-31 23:14:27 +0300

    refactor/assignment-1-oop

---
+++ Assignment_1_OOP/Season.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_OOP
+{
+    internal enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
