48f72454714dfd26b0359e3ba0dbac89e139224c
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-08-01 06:13:44 +0300

    Add code Question 02 & 03 in Part 02 to Assignment 3

---
+++ Assignment_3_OOP/Part 1 Mcq Question & Part 2/BasicAuthenticationService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1_Mcq_Question___Part_2
+{
+    internal class BasicAuthenticationService : IAuthenticationService
+    {
+        public Dictionary<string, string> usernameAndPassword = new Dictionary<string, string>()
+        {
+            {"admin","123"},
+            {"accounter","54875" },
+            {"hr","123"},
+            {"mentor","12345"},
+            {"instructor","456"}
+        };
+        public Dictionary<string, string> usernameAndRole = new Dictionary<string, string>()
+        {
+            {"admin","it"},
+            {"accounter","financial" },
+            {"hr","human resources"},
+            {"mentor","web development"},
+            {"instructor","web development"}
+        };
+        public bool AuthenticateUser(string username, string password)
+        {
+            if (usernameAndPassword.ContainsKey(username))
+            {
+                if (usernameAndPassword[username] == password)
+                    return true;
+            }
+            return false;
+        }
+
+        public bool AuthorizeUser(string username, string role)
+        {
+            if (usernameAndRole.ContainsKey(username))
+            {
+                if (usernameAndRole[username] == role)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assignment_3_OOP/Part 1 Mcq Question & Part 2/SmsNotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1_Mcq_Question___Part_2
+{
+    internal class SmsNotificationService : INotificationService
+    {
+        public void SendNotification(string recipient, string message)
+        {
+            Console.WriteLine($"Sms Notification to {recipient}: {message}");
+        }
+    }
+}
