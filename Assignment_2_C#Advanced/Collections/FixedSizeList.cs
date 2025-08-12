b61fb6b902872ee8750e8d6f4ef61182a9466eae
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-08-12 05:26:12 +0300

    Add_Assignment_2_C#Advanced

---
+++ Assignment_2_C#Advanced/Collections/FixedSizeList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class FixedSizeList<T>
+    {
+        private List<T> list;
+        private int counter;
+        public int Capacity { get; }
+
+        public FixedSizeList(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentException("Capacity must be greater than zero");
+
+            Capacity = capacity;
+            list = new List<T>();
+            counter = 0;
+        }
+        public void Add(T value)
+        {
+            if (counter >= Capacity)
+                throw new IndexOutOfRangeException();
+
+            list.Add(value);
+            counter++;
+        }
+        public T Get(int index)
+        {
+            if (index < Capacity && index >= 0)
+                return list[index];
+            else
+                throw new IndexOutOfRangeException();
+        }
+
+    }
+}
