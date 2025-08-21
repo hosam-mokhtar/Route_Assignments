5240a497e36907bf87444caa33c9b0f66d3c3817
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-08-21 22:00:42 +0300

    refactor/assignment-oop-2
    built Class SortPoint3D

---
+++ Assignment_2_OOP/First Project/SortPoint3D.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Project
+{
+    internal class SortPoint3D : IComparer
+    {
+        public int Compare(object? x, object? y)
+        {
+            if (x is not Point3D P1) return 1;
+            if (y is not Point3D P2) return -1;
+
+            int cmp = P1.X.CompareTo(P2.X);
+            if (cmp != 0)
+                return cmp;
+
+            return P1.Y.CompareTo(P2.Y);
+        }
+    }
+}
