0905ac155ceb4e1bb1ac387e0acdc655bd6d8337
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-08-21 23:26:06 +0300

    Add_Assignment_4_C#Advanced

---
+++ Assignment_4_C#Advanced/Delegate/Classes/BookFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.Classes
+{
+    internal class BookFunctions
+    {
+        public delegate string BookFunctionDelegate(Book book);
+        public static string GetISBN(Book b) => b.ISBN;
+        public static string GetTitle(Book b) => b.Title;
+        public static string GetAuthors(Book b) => string.Join(", ", b.Authors);
+        public static string GetPublicationDate(Book b) => b.PublicationDate.ToShortDateString();
+        public static string GetPrice(Book b) => $"Price: {b.Price}";
+
+
+    }
+}
---
+++ Assignment_4_C#Advanced/Delegate/Classes/LibraryEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static Delegate.Classes.BookFunctions;
+
+namespace Delegate.Classes
+{
+    internal class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList, /*BookFunctionDelegate*/ Func<Book, string> fptr)
+        {
+            if (bList is not null && fptr is not null)
+            {
+                foreach (Book B in bList)
+                {
+                    Console.WriteLine(fptr(B));
+                }
+            }
+        }
+    }
+
+}
