60e900ebd400b4b24ed3e5df12f81ba2fd267a3d
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-08-25 22:33:50 +0300

    Examination System

---
+++ Examination System/Examination/Exam/PracticalExam.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Examination.ConsoleHelper;
+
+namespace Examination
+{
+    internal class PracticalExam : Exam
+    {
+        public PracticalExam(int time, int numberOfQuestions) : base(time, numberOfQuestions)
+        {
+        }
+
+        public override void CreateListOfQuestions()
+        {
+            Questions = new MCQQuestion[NumberOfQuestions];
+
+            for (int i = 0; i < Questions?.Length; i++)
+            {
+                Questions[i] = new MCQQuestion();
+                Questions[i].AddQuestion();
+            }
+
+            Console.Clear();
+        }
+
+        public override void ShowExam()
+        {
+
+            foreach (var question in Questions)
+            {
+                Console.WriteLine(question);
+
+                for (int i = 0; i < question?.Answers?.Length; i++)
+                    Console.WriteLine(question.Answers[i]);
+
+                Console.WriteLine(new string('-',40));
+
+                string message = "Please Enter The answer Id";
+                question.UserAnswer.Id = ReadInt(message, min: 1, max: 3);
+                question.UserAnswer.Text = question.Answers[question.UserAnswer.Id - 1].Text;
+
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Right Answers");
+
+            for (int i = 0; i < Questions?.Length; i++)
+            {
+                Console.WriteLine($"Question Number {i + 1} : {Questions[i].Body}");
+                Console.WriteLine($"Your Answer => {Questions[i].UserAnswer.Text}");
+                Console.WriteLine($"Right Answer => {Questions[i].RightAnswer.Text}");
+
+                Console.WriteLine("-------------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ Examination System/Examination/ExamSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Examination.ConsoleHelper;
+
+namespace Examination
+{
+    internal static class ExamSystem
+    {
+        public static void Start()
+        {
+            Subject subject = new Subject(1, "OOP");
+
+            subject.CreateExam();
+            Console.Clear();
+
+            char Char;
+            do
+            {
+                Console.WriteLine("Do you want to  Start Exam (Y|N)");
+
+            } while (!(char.TryParse(Console.ReadLine(), out Char) && (Char is 'y' or 'Y' or 'n' or 'N')));
+
+
+            if (Char == 'Y' || Char == 'y')
+            {
+                Console.Clear();
+
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                subject.Exam.ShowExam();
+
+                sw.Stop();
+
+                Console.WriteLine($"Time = {sw.Elapsed}");
+            }
+
+            Console.WriteLine("Thank you");
+        }
+    }
+}
