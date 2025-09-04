0ce1c8e850956e5c2293c02af22696f09c7e4914
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-09-04 17:37:00 +0300

    Add_Assignment_1_EF Core

---
+++ Assignment_EF Core_1/EF Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Generic;
+
+namespace EF_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region   1.make a mapping for this schema(code first).
+            //Notes:-
+            //1.Don’t make relations between tables(create tables only).
+            //2.Use all ways of mapping(create a table using convention and another using fluent APIS and so on... ).
+
+            #endregion
+             
+        }
+    }
+}
