8ba2c907ebb0b77e7cef36bebbe14c90e1278901
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-09-10 12:22:44 +0300

    Add_Assignment_2_EF Core

---
+++ Assignment_EF Core_2/EF Core/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace EF_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignment 02
+            #region Create Relations Between Tables(Use Previous Assignment)
+
+            #endregion
+
+        }
+    }
+}
