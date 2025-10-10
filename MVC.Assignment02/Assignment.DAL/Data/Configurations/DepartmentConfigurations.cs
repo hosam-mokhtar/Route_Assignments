1bcda6904c8ddc592d50355fd67105740d0e143e
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-10-10 10:16:27 +0300

    Rename AssignmentName

---
+++ MVC.Assignment02/Assignment.DAL/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment.DAL.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(d => d.Id).UseIdentityColumn(10,10);
+
+            builder.Property(d => d.Name).HasColumnType("varchar(20)");
+            builder.Property(d => d.Code).HasColumnType("varchar(20)");
+            builder.Property(d => d.Description).HasColumnType("varchar(200)");
+
+
+            builder.Property(d => d.CreatedOn).HasDefaultValueSql("GETDATE()");               //insertion time only
+            builder.Property(d => d.LastModifiedOn).HasComputedColumnSql("GETDATE()");        //update time
+
+        }
+    }
+}
---
+++ MVC.Assignment02/Assignment.DAL/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment.DAL.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public int CreatedBy { get; set; }                     //User Id
+        public DateTime CreatedOn { get; set; }                //Insertion Date
+        public int LastModifiedBY { get; set; }                //User Id
+        public DateTime LastModifiedOn { get; set; }           //Modified Date
+        public bool IsDeleted { get; set; }                    //To Apply Soft Delete
+
+    }
+}
