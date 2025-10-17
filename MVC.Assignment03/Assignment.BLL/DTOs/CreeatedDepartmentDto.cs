d398731cdc77e515bafc4ca43b26a58da71c0d85
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-10-17 05:28:48 +0300

    Add_Assignment_3_MVC

---
+++ MVC.Assignment03/Assignment.BLL/DTOs/CreeatedDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.BLL.DTOs
+{
+    public class CreeatedDepartmentDto
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string? Description { get; set; }
+        public DateOnly DateOfCreation { get; set; }      // During insertion must add Time to DateTime
+    }
+}
---
+++ MVC.Assignment03/Assignment.DAL/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Assignment.DAL.Models;
+
+namespace Assignment.DAL.Data.Contexts
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Department> Departments { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connection string");
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Old Syntax
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            //or
+            //modelBuilder.ApplyConfiguration(new DepartmentConfigurations());
+
+
+
+            //New Syntax
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //or
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
