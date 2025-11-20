8ac65d7e236d5a6d924638a67735cab0a8069317
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-11-20 03:12:31 +0200

    Add_Assignment_6_APIs

---
+++ Talabat.Solution.06/Infrastructure/Persistence.Layer/DataSeeding.cs
@@ -0,0 +1,70 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Domain.Layer.Contracts;
+using Domain.Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Layer.Data;
+
+namespace Persistence.Layer
+{
+    public class DataSeeding(StoreDbContext _storeDbContext) : IDataSeeding
+    {
+
+        public async Task DataSeedAsync()
+        {
+            try
+            {
+                //Production
+                if ((await _storeDbContext.Database./*GetPendingMigrations()*/GetPendingMigrationsAsync()).Any())
+                {
+                    await _storeDbContext.Database.MigrateAsync();
+                }
+
+                if (!_storeDbContext.ProductTypes.Any())
+                {
+                    //var productTypessData = await File.ReadAllTextAsync(@"..\Infrastructure\Persistence.Layer\Data\DataSeed\types.json");
+                    var productTypessData = File.OpenRead(@"..\Infrastructure\Persistence.Layer\Data\DataSeed\types.json");      //OpenRead return FileStream
+                    //Convert string to C# Object
+                    var types = await JsonSerializer.DeserializeAsync<List<ProductType>>(productTypessData);
+
+                    if (types is not null && types.Any())
+                    {
+                        await _storeDbContext.ProductTypes.AddRangeAsync(types);
+                    }
+                }
+                if (!_storeDbContext.ProductBrands.Any())
+                {
+                    var productBrandsData = File.OpenRead(@"..\Infrastructure\Persistence.Layer\Data\DataSeed\brands.json");
+                    //Convert string to C# Object
+                    var brands = await JsonSerializer.DeserializeAsync<List<ProductBrand>>(productBrandsData);
+
+                    if (brands is not null && brands.Any())
+                    {
+                        await _storeDbContext.ProductBrands.AddRangeAsync(brands);
+                    }
+                }
+                if (!_storeDbContext.Products.Any())
+                {
+                    var productsData = File.OpenRead(@"..\Infrastructure\Persistence.Layer\Data\DataSeed\products.json");
+                    //Convert string to C# Object
+                    var products = await JsonSerializer.DeserializeAsync<List<Product>>(productsData);
+
+                    if (products is not null && products.Any())
+                    {
+                        await _storeDbContext.Products.AddRangeAsync(products);
+                    }
+                }
+
+                await _storeDbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                //todo:
+            }
+        }
+    }
+}
---
+++ Talabat.Solution.06/Infrastructure/Persistence.Layer/Repositories/GenericRepositories.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Layer.Contracts;
+using Domain.Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Persistence.Layer.Data;
+
+namespace Persistence.Layer.Repositories
+{
+    public class GenericRepositories<TEntity, TKey>(StoreDbContext _storeDbContext) : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+    {
+
+        #region Without Specifications
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        => await _storeDbContext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+                        => await _storeDbContext.Set<TEntity>().FindAsync(id);
+
+        #endregion
+
+        #region With Specifications
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(ISpecifications<TEntity, TKey> specifications)
+        {
+            //var baseQuery = _storeDbContext.Set<TEntity>();
+
+            //if (specifications is not null)
+            //{
+            //    var criteria = specifications.Criteria;
+            //    baseQuery = baseQuery.Where(criteria);
+            //}
+            //else if ()
+            //{
+            //}
+
+            return await SpecificationEvaluator.CreateQuery(_storeDbContext.Set<TEntity>(), specifications).ToListAsync();
+
+        }
+        public async Task<TEntity?> GetByIdAsync(ISpecifications<TEntity, TKey> specifications)
+        {
+            //var baseQuery = _storeDbContext.Set<TEntity>();
+
+            //if (specifications is not null)
+            //{
+            //    var criteria = specifications.Criteria;
+            //    baseQuery = baseQuery.Where(criteria);
+            //}
+            //else if ()
+            //{
+            //}
+
+            return await SpecificationEvaluator.CreateQuery(_storeDbContext.Set<TEntity>(), specifications).FirstOrDefaultAsync();
+        }
+
+        public async Task<int> CountAsync(ISpecifications<TEntity, TKey> specifications)
+        {
+            return await SpecificationEvaluator.CreateQuery(_storeDbContext.Set<TEntity>(), specifications).CountAsync(); ;
+        }
+
+        #endregion
+
+        public async Task AddAsync(TEntity entity)
+                      //=>  await _storeDbContext.AddAsync(entity);
+                        =>  await _storeDbContext.Set<TEntity>().AddAsync(entity);            //Readability
+       
+        public void Update(TEntity entity)
+                      //=> _storeDbContext.Update(entity);
+                        => _storeDbContext.Set<TEntity>().Update(entity);                     //Readability
+
+        public void Delete(TEntity entity)
+                      //=> _storeDbContext.Remove(entity);
+                        => _storeDbContext.Set<TEntity>().Remove(entity);                     //Readability
+
+    }
+}
